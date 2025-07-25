607ff9f4201f5b1a1aaff57822540c49c883e546
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-07-25 13:29:44 -0700

    Update tests with new Markdown loader

--- tests/test_util.py
+++ tests/test_util.py
@@ -20,15 +20,7 @@ def example_md():
 @pytest.fixture
 def example_md_meta():
     """Example Markdown metadata, raw."""
-    return 'title: "A Markdown example"'
-
-
-@pytest.fixture
-def example_md_body(example_md):
-    """Example Markdown body."""
-    lines = example_md.split("\n")
-    body = "\n".join(lines[3:]).strip()
-    return body
+    return {"title": "A Markdown example"}
 
 
 @pytest.fixture
@@ -39,14 +31,11 @@ def example_html():
     return html
 
 
-def test_split_yaml_body(example_md, example_md_meta, example_md_body):
-    """Test function to split YAML metadata block from body text."""
-    meta, body = util.split_yaml_body("example.md", example_md)
-    assert meta == example_md_meta
-    assert body == example_md_body
-
-
-def test_md2html(example_md, example_html):
+def test_md2html(example_md, example_md_meta, example_html):
     """Test Markdown to HTML conversion."""
-    _, body = util.split_yaml_body("example.md", example_md)
-    assert util.md2html(body) == example_html
+    data_meta = util.md2html("example.md", example_md, get_meta=True)
+    assert data_meta["meta"] == example_md_meta
+    assert data_meta["body"] == example_html
+    data_no_meta = util.md2html("example.md", example_md, get_meta=False)
+    assert data_no_meta["meta"] is None
+    assert data_no_meta["body"] == example_html
