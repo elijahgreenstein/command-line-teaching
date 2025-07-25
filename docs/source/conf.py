ad31320e96e93a5e9fb918ffcd5d020376463af8
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-07-25 13:31:44 -0700

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'CLT'
 copyright = '2025, Elijah Greenstein'
 author = 'Elijah Greenstein'
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
